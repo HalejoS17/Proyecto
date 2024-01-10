8a674cb04f2e8afb1ab34961961ebaf4eb96be97
ASUS <ASUS@SHA>
2024-01-09 20:30:15 -0500

    Agregar archivos de proyecto.

---
+++ ContactService.cs
@@ -0,0 +1,13 @@
+﻿using BlazorApp4.Models;
+
+namespace BlazorApp4
+{
+    public class ContactService
+    {
+        public List<Contact> ContactList = new List<Contact>();
+        public void AgregarContacto(Contact c)
+        {
+            ContactList.Add(c);
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorApp4.Models
+{
+    [Serializable]
+    public class Contact
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public String Email { get; set; }
+    }
+}
